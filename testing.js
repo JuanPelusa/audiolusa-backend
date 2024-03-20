13b078114192df10260ee45d1889eec8a9675f22
JuanPelusa <juan.ibaniez.oblanca@gmail.com>
2024-03-20 02:04:42 -0300

    desafio 2 Juan Ibañez

--- testing.js
+++ testing.js
@@ -1,28 +1,28 @@
 const productManager = require("./productManager");
 
-const producto = new productManager()
+const product = new productManager()
 
-console.log(producto.addProduct('Focal Maestro Utopia Evo', 'Speaker', 40000,'https://www.focal.com/es','hgi547uy', 5));
-console.log(producto.addProduct('Dali Epicon 8', 'Floorstanding loudspeaker', 15000,'https://www.dali-speakers.com/','jko802pf', 3));
-console.log(producto.addProduct('Dali Diablo Utopia Evo', 'Bookshelf 2-way loudspeaker', 12000,'https://www.dali-speakers.com/','kjh976gs', 3));
-console.log(producto.addProduct('Focal Sub Utopia EM Evo', 'Subwoofer', 25000,'https://www.focal.com/es','yio831cv', 3));
-console.log(producto.addProduct('Bowers & Wilkins 705 s3', 'Bookshelf 2-way loudspeaker', 3200,'https://www.bowerswilkins.com/en-us/','lfo467hg', 3));
+console.log(product.addProduct('Focal Maestro Utopia Evo', 'Speaker', 40000,'https://www.focal.com/es','hgi547uy', 5));
+console.log(product.addProduct('Dali Epicon 8', 'Floorstanding loudspeaker', 15000,'https://www.dali-speakers.com/','jko802pf', 3));
+console.log(product.addProduct('Dali Diablo Utopia Evo', 'Bookshelf 2-way loudspeaker', 12000,'https://www.dali-speakers.com/','kjh976gs', 3));
+console.log(product.addProduct('Focal Sub Utopia EM Evo', 'Subwoofer', 25000,'https://www.focal.com/es','yio831cv', 3));
+console.log(product.addProduct('Bowers & Wilkins 705 s3', 'Bookshelf 2-way loudspeaker', 3200,'https://www.bowerswilkins.com/en-us/','lfo467hg', 3));
 
 /* Get products test */
 
-//console.log(producto.getProducts());
+//console.log(product.getProducts());
 
 /* Get products by id test */
 
-//console.log(producto.getProductsById(1));
+//console.log(product.getProductsById(1));
 
 /* Get products by id "not found" test */
 
-//console.log(producto.getProductsById(9));
+//console.log(product.getProductsById(9));
 
 /* Delete product test */
 
-//console.log(producto.eraseProduct(5));
+//console.log(product.eraseProduct(5));
 
 
 /* Testing update */
@@ -35,4 +35,4 @@ console.log(producto.addProduct('Bowers & Wilkins 705 s3', 'Bookshelf 2-way loud
 //        price: 41000,
 //        image: 'https://www.bowerswilkins.com/en-us/'
 //    };
-//console.log(producto.updateProduct(1, updateTest));
+//console.log(product.updateProduct(1, updateTest));
